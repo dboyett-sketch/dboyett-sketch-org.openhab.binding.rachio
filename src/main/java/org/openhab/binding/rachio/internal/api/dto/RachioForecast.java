b05cc1a269e5dbe92bee384b88ed47ab763d704a
dboyett-sketch <dboyett@cox.net>
2025-12-04 21:27:52 -0600

    Update RachioForecast.java

--- src/main/java/org/openhab/binding/rachio/internal/api/dto/RachioForecast.java
+++ src/main/java/org/openhab/binding/rachio/internal/api/dto/RachioForecast.java
@@ -1,1 +1,39 @@
+package org.openhab.binding.rachio.internal.api.dto;
 
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.eclipse.jdt.annotation.Nullable;
+import com.google.gson.annotations.SerializedName;
+import java.util.List;
+
+@NonNullByDefault
+public class RachioForecast {
+    public String deviceId;
+    public String summary;
+    public List<ForecastDay> forecast;
+    
+    public static class ForecastDay {
+        @SerializedName("day")
+        public String day;
+        
+        @SerializedName("precip")
+        public double precipitation;
+        
+        @SerializedName("temp")
+        public double temperature;
+        
+        @SerializedName("humidity")
+        public double humidity;
+        
+        @SerializedName("wind")
+        public double windSpeed;
+        
+        @SerializedName("et")
+        public double evapotranspiration;
+        
+        @SerializedName("skip")
+        public boolean skip;
+        
+        @SerializedName("skipReason")
+        public @Nullable String skipReason;
+    }
+}
