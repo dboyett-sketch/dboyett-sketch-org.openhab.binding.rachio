cd40a6883e65f4cf655762227f30ea1f9a8e0f9a
dboyett-sketch <dboyett@cox.net>
2025-12-04 21:59:11 -0600

    Update CustomShade.java

--- src/main/java/org/openhab/binding/rachio/internal/api/dto/CustomShade.java
+++ src/main/java/org/openhab/binding/rachio/internal/api/dto/CustomShade.java
@@ -1,45 +1,84 @@
 package org.openhab.binding.rachio.internal.api.dto;
 
 import org.eclipse.jdt.annotation.NonNullByDefault;
-import org.eclipse.jdt.annotation.Nullable;
+import com.google.gson.annotations.SerializedName;
 
 /**
- * DTO for Rachio Custom Shade Configuration
- *
- * @author Daniel B. - Professional irrigation data
+ * DTO for Shade Type from Rachio API
+ * Based on Rachio professional irrigation data
+ * 
+ * @author Dave Boyett - Initial contribution
  */
 @NonNullByDefault
 public class CustomShade {
-    private String id = "";
-    private String name = "";
-    private @Nullable Double percentage; // 0-100%
+    // Shade identification
+    @SerializedName("id")
+    public String id;
     
-    public String getId() {
-        return id;
+    @SerializedName("name")
+    public String name;
+    
+    @SerializedName("custom")
+    public boolean custom;
+    
+    // Shade characteristics
+    @SerializedName("sunExposure")
+    public double sunExposure; // percentage (0-100)
+    
+    @SerializedName("evapotranspirationReduction")
+    public double evapotranspirationReduction; // percentage
+    
+    @SerializedName("waterRequirementFactor")
+    public double waterRequirementFactor; // multiplier (0-1)
+    
+    // Helper methods
+    
+    /**
+     * Get shade summary
+     */
+    public String getSummary() {
+        return String.format("%s (%.0f%% sun)", name, sunExposure);
     }
     
-    public void setId(String id) {
-        this.id = id;
+    /**
+     * Check if this is a custom shade type
+     */
+    public boolean isCustom() {
+        return custom;
     }
     
-    public String getName() {
-        return name;
+    /**
+     * Get adjusted water requirement
+     */
+    public double getAdjustedWaterRequirement(double baseRequirement) {
+        return baseRequirement * waterRequirementFactor;
     }
     
-    public void setName(String name) {
-        this.name = name;
+    /**
+     * Get adjusted ET value
+     */
+    public double getAdjustedET(double baseET) {
+        return baseET * (1.0 - (evapotranspirationReduction / 100.0));
     }
     
-    public @Nullable Double getPercentage() {
-        return percentage;
+    /**
+     * Get shade properties map
+     */
+    public java.util.Map<String, Object> getProperties() {
+        java.util.Map<String, Object> props = new java.util.HashMap<>();
+        props.put("id", id);
+        props.put("name", name);
+        props.put("custom", custom);
+        props.put("sunExposure", sunExposure);
+        props.put("evapotranspirationReduction", evapotranspirationReduction);
+        props.put("waterRequirementFactor", waterRequirementFactor);
+        return props;
     }
     
-    public void setPercentage(@Nullable Double percentage) {
-        this.percentage = percentage;
-    }
-    
-    @Override
-    public String toString() {
-        return "CustomShade{id='" + id + "', name='" + name + "', percentage=" + percentage + "}";
-    }
+    // Common shade type constants
+    public static final String NAME_FULL_SUN = "Full Sun";
+    public static final String NAME_PART_SUN = "Part Sun";
+    public static final String NAME_PART_SHADE = "Part Shade";
+    public static final String NAME_FULL_SHADE = "Full Shade";
+    public static final String NAME_DENSE_SHADE = "Dense Shade";
 }
