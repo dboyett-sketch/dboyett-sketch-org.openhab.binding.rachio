269d8c912712e56fa7eab09e9af9cffe3692a1dc
dboyett-sketch <dboyett@cox.net>
2025-12-07 23:59:05 -0600

    Update RachioWebHookEvent.java

--- src/main/java/org/openhab/binding/rachio/internal/api/dto/RachioWebHookEvent.java
+++ src/main/java/org/openhab/binding/rachio/internal/api/dto/RachioWebHookEvent.java
@@ -1,7 +1,5 @@
 package org.openhab.binding.rachio.internal.api.dto;
 
-import java.time.Instant;
-
 import org.eclipse.jdt.annotation.NonNullByDefault;
 import org.eclipse.jdt.annotation.Nullable;
 
@@ -11,90 +9,52 @@ import com.google.gson.annotations.SerializedName;
 public class RachioWebHookEvent {
     @SerializedName("type")
     private String type = "";
-    
+
     @SerializedName("subType")
-    private String subType = "";
-    
+    @Nullable
+    private String subType;
+
     @SerializedName("deviceId")
     private String deviceId = "";
-    
+
     @SerializedName("zoneId")
     @Nullable
     private String zoneId;
-    
+
     @SerializedName("eventId")
-    private String eventId = "";
-    
-    @SerializedName("createDate")
-    private Instant createDate;
-    
+    @Nullable
+    private String eventId;
+
     @SerializedName("summary")
     @Nullable
-    private String summary;
-    
+    private RachioEventSummary summary;
+
     // Getters
     public String getType() {
         return type;
     }
-    
+
+    @Nullable
     public String getSubType() {
         return subType;
     }
-    
+
     public String getDeviceId() {
         return deviceId;
     }
-    
+
     @Nullable
     public String getZoneId() {
         return zoneId;
     }
-    
+
+    @Nullable
     public String getEventId() {
         return eventId;
     }
-    
-    public Instant getCreateDate() {
-        return createDate;
-    }
-    
+
     @Nullable
-    public String getSummary() {
+    public RachioEventSummary getSummary() {
         return summary;
-    }
-    
-    // Setters
-    public void setType(String type) {
-        this.type = type;
-    }
-    
-    public void setSubType(String subType) {
-        this.subType = subType;
-    }
-    
-    public void setDeviceId(String deviceId) {
-        this.deviceId = deviceId;
-    }
-    
-    public void setZoneId(@Nullable String zoneId) {
-        this.zoneId = zoneId;
-    }
-    
-    public void setEventId(String eventId) {
-        this.eventId = eventId;
-    }
-    
-    public void setCreateDate(Instant createDate) {
-        this.createDate = createDate;
-    }
-    
-    public void setSummary(@Nullable String summary) {
-        this.summary = summary;
-    }
-    
-    @Override
-    public String toString() {
-        return "RachioWebHookEvent [type=" + type + ", subType=" + subType + ", deviceId=" + deviceId 
-                + ", zoneId=" + zoneId + ", eventId=" + eventId + "]";
     }
 }
