11769cc805455c68372198bb45d8ce99c4b378ff
dboyett-sketch <dboyett@cox.net>
2025-12-08 19:56:57 -0600

    Update RachioApiWebHookEntry.java

--- src/main/java/org/openhab/binding/rachio/internal/api/RachioApiWebHookEntry.java
+++ src/main/java/org/openhab/binding/rachio/internal/api/RachioApiWebHookEntry.java
@@ -3,40 +3,13 @@ package org.openhab.binding.rachio.internal.api;
 import org.eclipse.jdt.annotation.NonNullByDefault;
 import org.eclipse.jdt.annotation.Nullable;
 
-import com.google.gson.annotations.SerializedName;
-
 @NonNullByDefault
 public class RachioApiWebHookEntry {
-    @SerializedName("id")
-    private String id = "";
-
-    @SerializedName("url")
-    private String url = "";
-
-    @SerializedName("externalId")
-    @Nullable
-    private String externalId;
-
-    @SerializedName("eventTypes")
-    @Nullable
-    private String[] eventTypes;
-
-    // Getters
-    public String getId() {
-        return id;
-    }
-
-    public String getUrl() {
-        return url;
-    }
-
-    @Nullable
-    public String getExternalId() {
-        return externalId;
-    }
-
-    @Nullable
-    public String[] getEventTypes() {
-        return eventTypes;
-    }
+    public @Nullable String id;
+    public @Nullable String url;
+    public @Nullable String externalId;
+    
+    public @Nullable String getId() { return id; }
+    public @Nullable String getUrl() { return url; }
+    public @Nullable String getExternalId() { return externalId; }
 }
