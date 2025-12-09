2eedf8813d49c9362bb2dcdd46892bea624107ff
dboyett-sketch <dboyett@cox.net>
2025-12-08 18:54:32 -0600

    Update CustomSoil.java

--- src/main/java/org/openhab/binding/rachio/internal/api/dto/CustomSoil.java
+++ src/main/java/org/openhab/binding/rachio/internal/api/dto/CustomSoil.java
@@ -1,23 +1,15 @@
 package org.openhab.binding.rachio.internal.api.dto;
 
 import org.eclipse.jdt.annotation.NonNullByDefault;
-
-import com.google.gson.annotations.SerializedName;
+import org.eclipse.jdt.annotation.Nullable;
 
 @NonNullByDefault
 public class CustomSoil {
-    @SerializedName("name")
-    private String name = "";
-
-    @SerializedName("availableWater")
-    private double availableWater = 0.0;
-
-    // Getters
-    public String getName() {
-        return name;
-    }
-
-    public double getAvailableWater() {
-        return availableWater;
-    }
+    public @Nullable String id;
+    public @Nullable String name;
+    public @Nullable Double availableWater;
+    
+    public @Nullable String getId() { return id; }
+    public @Nullable String getName() { return name; }
+    public @Nullable Double getAvailableWater() { return availableWater; }
 }
