c047e4e727b3c7d117cb564dc077f28501928713
dboyett-sketch <dboyett@cox.net>
2025-12-08 19:05:18 -0600

    Update RachioStatusListener.java

--- src/main/java/org/openhab/binding/rachio/internal/handler/RachioStatusListener.java
+++ src/main/java/org/openhab/binding/rachio/internal/handler/RachioStatusListener.java
@@ -1,6 +1,6 @@
 package org.openhab.binding.rachio.internal.handler;
 
-import java.time.Instant;
+import java.util.Collection;
 
 import org.eclipse.jdt.annotation.NonNullByDefault;
 import org.eclipse.jdt.annotation.Nullable;
@@ -8,39 +8,24 @@ import org.openhab.binding.rachio.internal.api.dto.RachioDevice;
 import org.openhab.binding.rachio.internal.api.dto.RachioWebHookEvent;
 
 /**
- * Interface for listening to status changes from Rachio devices
- * 
+ * Listener interface for Rachio status updates
+ *
  * @author Damion Boyett - Initial contribution
  */
 @NonNullByDefault
 public interface RachioStatusListener {
+    /**
+     * Called when the device list is updated
+     */
+    void deviceListUpdated(Collection<RachioDevice> devices);
     
     /**
-     * Called when a device status changes via webhook event
-     * 
-     * @param event the webhook event
+     * Called when a specific device is updated
      */
-    void onStatusChanged(RachioWebHookEvent event);
+    void deviceUpdated(RachioDevice device);
     
     /**
-     * Called when a device is updated (refreshed from API)
-     * 
-     * @param device the updated device
+     * Called when a webhook event is received
      */
-    void onDeviceUpdated(RachioDevice device);
-    
-    /**
-     * Get the device ID that this listener is interested in
-     * 
-     * @return the device ID, or empty string for all devices
-     */
-    String getDeviceId();
-    
-    /**
-     * Get the last event time for this listener
-     * 
-     * @return the last event time, or null if no events received
-     */
-    @Nullable
-    Instant getLastEventTime();
+    void webhookEventReceived(RachioWebHookEvent event);
 }
