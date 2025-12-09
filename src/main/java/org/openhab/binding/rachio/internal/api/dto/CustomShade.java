6a05f81418e6f0120fe54827dfb48036934b71d8
dboyett-sketch <dboyett@cox.net>
2025-12-09 01:14:20 -0600

    Update CustomShade.java

--- src/main/java/org/openhab/binding/rachio/internal/api/dto/CustomShade.java
+++ src/main/java/org/openhab/binding/rachio/internal/api/dto/CustomShade.java
@@ -3,11 +3,27 @@ package org.openhab.binding.rachio.internal.api.dto;
 import org.eclipse.jdt.annotation.NonNullByDefault;
 import org.eclipse.jdt.annotation.Nullable;
 
+/**
+ * DTO for Custom Shade data
+ *
+ * @author David Boyett - Initial contribution
+ */
 @NonNullByDefault
 public class CustomShade {
-    public @Nullable String id;
-    public @Nullable String name;
+    @Nullable
+    public String id;
     
-    public @Nullable String getId() { return id; }
-    public @Nullable String getName() { return name; }
+    @Nullable
+    public String name; // FULL_SUN, PART_SHADE, FULL_SHADE
+    
+    @Nullable
+    public Double sunExposure; // percentage
+    
+    @Nullable
+    public String description;
+    
+    @Override
+    public String toString() {
+        return "CustomShade [name=" + name + ", sunExposure=" + sunExposure + "]";
+    }
 }
