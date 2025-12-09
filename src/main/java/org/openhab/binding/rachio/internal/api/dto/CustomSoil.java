f2bf2045fc50cf32904fee400be033c376d13503
dboyett-sketch <dboyett@cox.net>
2025-12-09 01:10:40 -0600

    Update CustomSoil.java

--- src/main/java/org/openhab/binding/rachio/internal/api/dto/CustomSoil.java
+++ src/main/java/org/openhab/binding/rachio/internal/api/dto/CustomSoil.java
@@ -3,13 +3,30 @@ package org.openhab.binding.rachio.internal.api.dto;
 import org.eclipse.jdt.annotation.NonNullByDefault;
 import org.eclipse.jdt.annotation.Nullable;
 
+/**
+ * DTO for Custom Soil data
+ *
+ * @author David Boyett - Initial contribution
+ */
 @NonNullByDefault
 public class CustomSoil {
-    public @Nullable String id;
-    public @Nullable String name;
-    public @Nullable Double availableWater;
+    @Nullable
+    public String id;
     
-    public @Nullable String getId() { return id; }
-    public @Nullable String getName() { return name; }
-    public @Nullable Double getAvailableWater() { return availableWater; }
+    @Nullable
+    public String name; // SANDY, CLAY, LOAM, etc.
+    
+    @Nullable
+    public Double availableWater; // inches per foot
+    
+    @Nullable
+    public Double infiltrationRate; // inches per hour
+    
+    @Nullable
+    public String description;
+    
+    @Override
+    public String toString() {
+        return "CustomSoil [name=" + name + ", availableWater=" + availableWater + "]";
+    }
 }
