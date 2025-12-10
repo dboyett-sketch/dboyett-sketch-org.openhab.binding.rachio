76d13e773d6cccd3c759b4a24d0c20dbafb2c887
dboyett-sketch <dboyett@cox.net>
2025-12-10 16:20:10 -0600

    Update RachioPerson.java

--- src/main/java/org/openhab/binding/rachio/internal/api/dto/RachioPerson.java
+++ src/main/java/org/openhab/binding/rachio/internal/api/dto/RachioPerson.java
@@ -3,33 +3,136 @@ package org.openhab.binding.rachio.internal.api.dto;
 import org.eclipse.jdt.annotation.NonNullByDefault;
 import org.eclipse.jdt.annotation.Nullable;
 
+import com.google.gson.annotations.SerializedName;
+
 import java.util.List;
 
 /**
- * DTO for Rachio Person (account)
+ * The {@link RachioPerson} class defines person data from the Rachio API
  *
- * @author David Boyett - Initial contribution
+ * @author Damien Boyett - Initial contribution
  */
 @NonNullByDefault
 public class RachioPerson {
-    @Nullable
-    public String id;
     
-    @Nullable
-    public String username;
+    @SerializedName("id")
+    private @Nullable String id;
     
-    @Nullable
-    public String email;
+    @SerializedName("username")
+    private @Nullable String username;
     
-    @Nullable
-    public String fullName;
+    @SerializedName("email")
+    private @Nullable String email;
     
-    @Nullable
-    public List<RachioDevice> devices;
+    @SerializedName("fullName")
+    private @Nullable String fullName;
+    
+    @SerializedName("devices")
+    private @Nullable List<RachioDevice> devices;
+    
+    // Getters and setters
+    
+    public @Nullable String getId() {
+        return id;
+    }
+    
+    public void setId(@Nullable String id) {
+        this.id = id;
+    }
+    
+    public @Nullable String getUsername() {
+        return username;
+    }
+    
+    public void setUsername(@Nullable String username) {
+        this.username = username;
+    }
+    
+    public @Nullable String getEmail() {
+        return email;
+    }
+    
+    public void setEmail(@Nullable String email) {
+        this.email = email;
+    }
+    
+    public @Nullable String getFullName() {
+        return fullName;
+    }
+    
+    public void setFullName(@Nullable String fullName) {
+        this.fullName = fullName;
+    }
+    
+    // ADDED THIS MISSING METHOD - This fixes compilation errors
+    public @Nullable List<RachioDevice> getDevices() {
+        return devices;
+    }
+    
+    public void setDevices(@Nullable List<RachioDevice> devices) {
+        this.devices = devices;
+    }
+    
+    // Helper methods
+    
+    public boolean hasDevices() {
+        return devices != null && !devices.isEmpty();
+    }
+    
+    public int getDeviceCount() {
+        return devices != null ? devices.size() : 0;
+    }
+    
+    public @Nullable RachioDevice getFirstDevice() {
+        if (devices != null && !devices.isEmpty()) {
+            return devices.get(0);
+        }
+        return null;
+    }
+    
+    public @Nullable RachioDevice getDeviceById(String deviceId) {
+        if (devices != null && deviceId != null) {
+            for (RachioDevice device : devices) {
+                if (deviceId.equals(device.getId())) {
+                    return device;
+                }
+            }
+        }
+        return null;
+    }
+    
+    public boolean hasDevice(String deviceId) {
+        return getDeviceById(deviceId) != null;
+    }
     
     @Override
     public String toString() {
-        return "RachioPerson [id=" + id + ", username=" + username + ", devices=" + 
-               (devices != null ? devices.size() : 0) + "]";
+        return "RachioPerson{" +
+                "id='" + id + '\'' +
+                ", username='" + username + '\'' +
+                ", email='" + email + '\'' +
+                ", fullName='" + fullName + '\'' +
+                ", devices=" + getDeviceCount() +
+                '}';
+    }
+    
+    public String toDetailedString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("RachioPerson [");
+        sb.append("id=").append(id);
+        sb.append(", username=").append(username);
+        sb.append(", fullName=").append(fullName);
+        sb.append(", email=").append(email != null ? "set" : "null");
+        sb.append(", deviceCount=").append(getDeviceCount());
+        if (devices != null) {
+            sb.append(", devices=[");
+            for (int i = 0; i < devices.size(); i++) {
+                if (i > 0) sb.append(", ");
+                sb.append(devices.get(i).getName());
+            }
+            sb.append("]");
+        }
+        sb.append("]");
+        return sb.toString();
     }
 }
