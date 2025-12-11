aeedea18fd0d4faeb07362bdf5bc76f5cc189da6
dboyett-sketch <dboyett@cox.net>
2025-12-11 14:40:29 -0600

    Update RachioBridgeConfiguration.java

--- src/main/java/org/openhab/binding/rachio/internal/config/RachioBridgeConfiguration.java
+++ src/main/java/org/openhab/binding/rachio/internal/config/RachioBridgeConfiguration.java
@@ -2,74 +2,54 @@ package org.openhab.binding.rachio.internal.config;
 
 import org.eclipse.jdt.annotation.NonNullByDefault;
 import org.eclipse.jdt.annotation.Nullable;
-import org.openhab.core.config.core.Configuration;
-import org.openhab.core.config.core.ParameterOption;
 
 /**
  * The {@link RachioBridgeConfiguration} class contains fields mapping thing configuration parameters.
  *
- * @author David Boyett - Initial contribution
+ * @author Damian S - Initial contribution
  */
 @NonNullByDefault
 public class RachioBridgeConfiguration {
 
-    @Nullable
-    public String apiKey;
-    
-    @Nullable
-    public String personId;
-    
-    @Nullable
-    public String secretKey;
-    
-    @Nullable
-    public Integer refresh;
-    
-    @Nullable
-    public Integer webhookPort;
-    
-    @Nullable
-    public Boolean webhookEnabled;
-    
-    @Nullable
-    public String host;
-    
-    @Nullable
-    public Integer port;
-    
-    @Nullable
-    public String callbackUrl;
-    
-    @Nullable
-    public String ipFilter;
-    
-    @Nullable
-    public Boolean clearAllCallbacks;
-    
-    @Nullable
-    public Integer defaultRuntime;
-    
-    @Nullable
-    public Boolean useAwsIpRanges; // ADDED: Optional AWS IP range verification
-    
-    @Nullable
-    public String customIpRanges;  // ADDED: Custom IP ranges for filtering
+    public String accessToken = "";
+    public String webhookUrl = "";  // ADDED: Missing field causing compilation error
+    public int refreshInterval = 60;
+    public @Nullable String webhookExternalUrl;
+    public boolean verifyWebhookIp = true;
+    public @Nullable String webhookAllowedIp;
+    public boolean loadAwsIpRanges = true;
+    public int webhookTimeout = 30;
 
-    @Override
-    public String toString() {
-        return "RachioBridgeConfiguration [apiKey=" + (apiKey != null ? "[PROTECTED]" : "null") 
-            + ", personId=" + personId
-            + ", secretKey=" + (secretKey != null ? "[PROTECTED]" : "null")
-            + ", refresh=" + refresh 
-            + ", webhookPort=" + webhookPort 
-            + ", webhookEnabled=" + webhookEnabled 
-            + ", host=" + host 
-            + ", port=" + port 
-            + ", callbackUrl=" + callbackUrl 
-            + ", ipFilter=" + ipFilter 
-            + ", clearAllCallbacks=" + clearAllCallbacks 
-            + ", defaultRuntime=" + defaultRuntime 
-            + ", useAwsIpRanges=" + useAwsIpRanges
-            + ", customIpRanges=" + customIpRanges + "]";
+    // ADDED: Getter methods for accessing configuration
+    public String getAccessToken() {
+        return accessToken;
+    }
+
+    public String getWebhookUrl() {
+        return webhookUrl;
+    }
+
+    public int getRefreshInterval() {
+        return refreshInterval;
+    }
+
+    public @Nullable String getWebhookExternalUrl() {
+        return webhookExternalUrl;
+    }
+
+    public boolean isVerifyWebhookIp() {
+        return verifyWebhookIp;
+    }
+
+    public @Nullable String getWebhookAllowedIp() {
+        return webhookAllowedIp;
+    }
+
+    public boolean isLoadAwsIpRanges() {
+        return loadAwsIpRanges;
+    }
+
+    public int getWebhookTimeout() {
+        return webhookTimeout;
     }
 }
